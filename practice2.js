c6c5f7ceefa14d31521532512215378ebc7a24a1
Sami Hussain <161669469+samihussinpk@users.noreply.github.com>
2024-09-09 03:22:01 +0500

    Add files via upload

---
+++ practice2.js
@@ -0,0 +1,19 @@
+//                    DESTRUCTURING IN ARRAY 
+//   OBJECT1
+let applicant1 = {
+    firstName: "Sami",
+    lastName: "Hussain",
+    age: 22,
+    qualification: "BSIT",
+    cnic: 1234567890
+};
+//    OBJECT2
+let applicant2 = {
+    firstName: "Sami",
+    lastName: "Hussain",
+    age: 22,
+    qualification: "BSIT",
+    cnic: 1234567890
+};
+console.log(applicant1, applicant2);
+export {};
---
+++ practice2.ts
@@ -0,0 +1,62 @@
+//                    DESTRUCTURING IN ARRAY 
+
+// Array
+// let arr = ["Bilal",40,"samixyz.gmail.com"]
+
+// // Destrucring 
+// let [name1,age,email] = arr
+// console.log(name1);
+//              ...........................................
+//     ARRAY
+// let friends: string [] = ["Sami","Arsalan","Noman","Salman","Saad"];
+
+//     DESTRUCTURING
+// let [friend1,friend2,friend3,friend4,friend5] = friends
+// console.log(friend2);
+
+//.............................................................................
+//     DESTRUCTURING IN OBJECT
+
+//         OBJECT
+// let data = {
+//         name: "John",
+//         fatherName: "Doe",
+//         age: 35,
+//         email:"Johnxyz.gmail.com",
+//         hobbies: ["Reading","Listening to songs","Hiking"]
+// };
+
+// //         DECONSTRUCTION
+// let {name,fatherName,age,hobbies} = data;
+// console.log(hobbies);
+//..................................................................
+//      INTERFACE    <----  interface is used to define type of  an object
+
+//     INTERFACE
+// interface requirements {
+//         firstName: string
+//         lastName: string
+//         age: number
+//         qualification: string
+//         cnic: number
+// }
+
+//       OBJECT1
+// let applicant1: requirements = {
+//         firstName:"Sami",
+//         lastName:"Hussain",
+//         age:22,
+//         qualification: "BSIT",
+//         cnic:1234567890
+// }
+
+//        OBJECT2
+// let applicant2: requirements = {
+//         firstName:"Sami",
+//         lastName:"Hussain",
+//         age:22,
+//         qualification: "BSIT",
+//         cnic:1234567890
+// }
+
+// console.log(applicant1,applicant2)
