7a9d6849dbf33986c8911cbb43d06180d1074685
Sami Hussain <161669469+samihussinpk@users.noreply.github.com>
2024-09-13 13:47:57 +0500

    Add files via upload

---
+++ practiceOfInterface.js
@@ -0,0 +1,15 @@
+//INTERFACE <--- we use interface to define the type of an object.
+// we used here "studentData" to define the type of an object
+let student1 = {
+    name: "Albert",
+    fatherName: "John",
+    id: 1111
+};
+// we used here "studentData" to define the type of an object
+let student2 = {
+    name: "bob",
+    fatherName: "Doe",
+    id: 2222
+};
+console.log(student1, "\n", student2);
+export {};
---
+++ practiceOfInterface.ts
@@ -0,0 +1,57 @@
+//INTERFACE <--- we use interface to define the type of an object.
+
+//   interface
+// interface studentData {
+//     name: string,
+//     fatherName: string,
+//     id: number,
+// }
+
+// we used here "studentData" to define the type of an object
+// let student1: studentData = {
+//     name:"Albert",
+//     fatherName:"John",
+//     id: 1111
+
+// }
+
+// we used here "studentData" to define the type of an object
+// let student2: studentData = {
+//     name: "bob",
+//     fatherName:"Doe",
+//     id: 2222
+    
+// }
+// console.log(student1,"\n",student2);
+// .....................................................
+//  INTERFACE AND EXTENDS
+
+// interface studentData {
+//     name: string,
+//     fatherName: string,
+//     id: number,
+// }
+
+// // we used the properties of studentData here with the help of "extend"
+// interface employeeData extends studentData{
+//     email: string
+//     contact: number
+
+// }
+
+// let student1: studentData = {
+//         name:"Albert",
+//         fatherName:"John",
+//         id: 1111
+    
+//     }
+
+// let employee1 : employeeData = {
+//     name:"david",
+//     fatherName:"diod",
+//     id: 5555,
+//     email: "david@.gmail.com",
+//     contact: 234354676
+// }    
+
+// console.log(student1,"\n",employee1);
