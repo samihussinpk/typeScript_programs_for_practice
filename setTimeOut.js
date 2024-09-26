5e36e4f8c9a3efa193ac0c7673edab9b3237c4ac
Sami Hussain <161669469+samihussinpk@users.noreply.github.com>
2024-09-26 22:38:37 +0500

    Add files via upload

---
+++ setTimeOut.js
@@ -0,0 +1,11 @@
+//setTimeOut  <---  it is used to return promise and to set time delay.
+setTimeout(function () {
+    console.log("Welcome");
+}, 1000);
+setTimeout(function () {
+    console.log("Mr. Sami");
+}, 2000);
+setTimeout(function () {
+    console.log("Son of Ghulam Hussain");
+}, 3000);
+export {};
